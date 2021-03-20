b435cdcce8713d529230f9dddec0a074aee7fbb6
Igor do E. Santo <igorbalbinox1@hotmail.com>
2021-03-20 00:04:09 -0300

    criado arquivo home.vue para criacao da tela inicial.

---
+++ src/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home_page">
+      
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home_page {
+    height: 1080px;
+    width: 1920px;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
